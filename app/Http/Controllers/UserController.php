c6ee887c799f760e06f612d0e43d59b4855a43f7
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2023-06-15 20:39:03 -0300

    Login y registro completo

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    public function Register(Request $request){
+        $u = new User();
+        $u -> name = $request -> post("username");
+        $u -> email = $request -> post("email");
+        $u -> password = Hash::make($request -> post("password"));
+
+        $u -> save();
+
+        return redirect("/login")-> with("created",true);
+
+    }
+
+    public function Login(Request $request){
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) 
+            return redirect("/");
+        return redirect("/login")->with("failed",true);
+    }
+
+    public function Logout(Request $request){
+        Auth::logout();
+        return redirect("/login")->with("logout",true);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
 });
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::post('/login',[UserController::class,"Login"]);
+Route::post('/register',[UserController::class,"Register"]);
+Route::get('/logout',[UserController::class,"Logout"]);
